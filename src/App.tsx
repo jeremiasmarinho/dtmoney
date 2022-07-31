06d83acc6f49e76c66d4f6ecaa934673875c2c95
Jeremias Marinho <jeremias10@msn.com>
2022-07-31 05:53:21 -0300

    Config Css Glogal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>Jesus Cristo</Title>
+      <h1>Jesus Cristo</h1>
+      <GlobalStyle />
     </div>
   );
 };
